187a8b16ca1b458b9c42b440fdba4112f967bbe8
Titti M <tmalmivi@cs.helsinki.fi>
2014-01-21 13:18:22 +0200

    vko2 t5

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,6 +3,7 @@ class Beer < ActiveRecord::Base
   has_many :ratings
 
   def average_rating
-    ratings.average('score').to_f
+    #ratings.average('score').to_f
+    ratings.inject(0) {|n, r| n + r.score}.to_f / ratings.count
   end  
 end
