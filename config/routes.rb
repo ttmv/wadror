f54f6815b66bf8c04ab23d611fe07c5783801994
Titti M <tmalmivi@cs.helsinki.fi>
2014-01-22 15:08:21 +0200

    vko2 t11

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Ratebeer::Application.routes.draw do
 
   root 'breweries#index'
 
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to:'ratings#new'
-  post 'ratings', to: 'ratings#create'
+  #get 'ratings', to: 'ratings#index'
+  #get 'ratings/new', to:'ratings#new'
+  #post 'ratings', to: 'ratings#create'
+
+  resources :ratings, :only => [:index, :new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
