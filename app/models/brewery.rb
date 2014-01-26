525034207a2a7d443a1dd869ebf4f68566f63fbe
Titti M <tmalmivi@cs.helsinki.fi>
2014-01-27 00:16:48 +0200

    vko3 t1 jne

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,18 @@
 class Brewery < ActiveRecord::Base
+  include RatingAverage
+
   has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
 
-  include RatingAverage
+  def print_report
+    puts name
+    puts "established at year #{year}"
+    puts "number of beers #{beers.count}"
+    puts "number of ratings #{ratings.count}"
+  end  
+
+  def restart
+    self.year = 2014
+    puts "changed year to #{year}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Ratebeer::Application.routes.draw do
+  resources :users
   resources :beers
-
   resources :breweries
-
   root 'breweries#index'
-
-  #get 'ratings', to: 'ratings#index'
-  #get 'ratings/new', to:'ratings#new'
-  #post 'ratings', to: 'ratings#create'
-
   resources :ratings, :only => [:index, :new, :create, :destroy]
+  
+  get 'signup', to: 'users#new'
+
+  resources :sessions, only: [:new, :create, :destroy]  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
