6c2994409e09f9e10ff3792297681217508dd6bf
Titti M <tmalmivi@cs.helsinki.fi>
2014-01-28 18:31:30 +0200

    vko3 t12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,6 +59,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy if @user == current_user
+    session[:user_id] = nil
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
