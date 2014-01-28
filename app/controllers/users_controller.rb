40ea1f9e7c63cecc387fe813b2148808edfe1f6d
Titti M <tmalmivi@cs.helsinki.fi>
2014-01-28 18:10:10 +0200

    vko3 t11&t13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,21 +40,25 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    if @user == current_user
+      respond_to do |format|
+        if @user.update(user_params) 
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to signin_path
     end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @user.destroy if @user == current_user
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   include RatingAverage
   
-  has_many :ratings
+  has_many :ratings, :dependent => :destroy
   has_many :beers, through: :ratings
 
   has_secure_password
