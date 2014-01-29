55682a4d64b56e5597d5932dcddda3965bd286df
Titti M <tmalmivi@cs.helsinki.fi>
2014-01-29 16:39:22 +0200

    vko3 t6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,8 +5,15 @@ class Brewery < ActiveRecord::Base
   has_many :ratings, :through => :beers
   validates :name, presence: true
   validates :year, numericality: {greater_than_or_equal_to: 1042,
-                                  less_than_or_equal_to: 2014,
                                   only_integer: true }
+  validate :year_not_later_than_current
+
+
+  def year_not_later_than_current
+    if year > Time.now.year
+      errors.add(:year, "cannot be later than #{Time.now.year}")
+    end
+  end
 
   def print_report
     puts name
