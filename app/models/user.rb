f2caa30e11c7f1188ae559c2ecb7971202629137
Titti M <tmalmivi@cs.helsinki.fi>
2014-01-31 13:12:10 +0200

    vko 3 t7-8 models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   
   has_many :ratings, :dependent => :destroy
   has_many :beers, through: :ratings
+  has_many :memberships, :dependent => :destroy
+  has_many :beer_clubs, through: :memberships
 
   has_secure_password
 
