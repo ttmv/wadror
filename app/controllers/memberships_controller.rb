1e8ec5ece5687bd754c8ce0452682979baff3afe
Titti M <tmalmivi@cs.helsinki.fi>
2014-02-01 02:04:32 +0200

    loput ysist

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -15,7 +15,7 @@ class MembershipsController < ApplicationController
   # GET /memberships/new
   def new
     @membership = Membership.new
-    @beerclubs = BeerClub.all
+    @beer_clubs = BeerClub.all
   end
 
   # GET /memberships/1/edit
@@ -33,6 +33,7 @@ class MembershipsController < ApplicationController
           format.html { redirect_to @membership, notice: 'Membership was successfully created.' }
           format.json { render action: 'show', status: :created, location: @membership }
         else
+          @beer_clubs = BeerClub.all
           format.html { render action: 'new' }
           format.json { render json: @membership.errors, status: :unprocessable_entity }
         end
