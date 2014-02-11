32b6c905daf4807eb67e85f2cb356a92f516b64e
Titti M <tmalmivi@cs.helsinki.fi>
2014-02-11 17:37:18 +0200

    vko5 t4 ja korjauksia aiempiin

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,12 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 1.week) {fetch_places_in(city)}
+  end
+
+  private
+
+  def self.fetch_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
