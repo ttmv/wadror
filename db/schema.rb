eac64c50111022439782207f069e1fb53dc13e1f
Titti M <tmalmivi@cs.helsinki.fi>
2014-02-14 17:06:03 +0200

    old_style pois

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140214134616) do
+ActiveRecord::Schema.define(version: 20140214150331) do
 
   create_table "beer_clubs", force: true do |t|
     t.string   "name"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20140214134616) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
-    t.string   "old_style"
     t.integer  "brewery_id"
     t.datetime "created_at"
     t.datetime "updated_at"
