28dd8dce2aa2e3a5cb56a0b1934f9de877fe42d2
Titti M <tmalmivi@cs.helsinki.fi>
2014-02-14 15:56:57 +0200

    beer&style liittäminen

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,12 +1,13 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  belongs_to :style
   has_many :ratings, :dependent => :destroy
   has_many :raters, through: :ratings, source: :user
 
   include RatingAverage
   
   validates :name, presence: true
-  validates :style, presence: true
+  #validates :style, presence: true
 
   def to_s
     "#{name}; #{brewery.name}"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140214125026) do
+ActiveRecord::Schema.define(version: 20140214134616) do
 
   create_table "beer_clubs", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140214125026) do
     t.integer  "brewery_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "style_id"
   end
 
   create_table "breweries", force: true do |t|
