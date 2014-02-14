ca820f849ab5977d076cc8892ab850617908fcbd
Titti M <tmalmivi@cs.helsinki.fi>
2014-02-14 14:18:32 +0200

    taulu stylelle

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211160744) do
+ActiveRecord::Schema.define(version: 20140214115344) do
 
   create_table "beer_clubs", force: true do |t|
     t.string   "name"
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 20140211160744) do
   end
 
   add_index "settings", ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true
+
+  create_table "styles", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
